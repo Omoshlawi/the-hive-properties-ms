3fe8087a407838f06e66db529f17946e5cfbb9c0
Omoshlawi <lawiomosh3@gmail.com>
2023-12-21 16:33:49 +0300

    Done updating listing models to address errors and optimize, sales and rental listing not complete though

--- listings/models.py
+++ listings/models.py
@@ -9,10 +9,10 @@ from listings.utils import listing_images
 # Create your models here.
 
 
-
-
-
 class Listing(PublishableBaseModel):
+    """
+    Object that make property or properties available for rent, sale, lease, e.t.c
+    """
     title = models.CharField(max_length=255)
     slug = models.SlugField(unique=True, db_index=True)
     description = models.TextField(null=True, blank=True)
@@ -22,24 +22,19 @@ class Listing(PublishableBaseModel):
     featured = models.BooleanField(default=False)
     tags = TaggableManager()
     amenities = models.TextField(null=True, blank=True, help_text="Comma separated amenities")
+    cover_image = models.ImageField(null=False, blank=False, upload_to=listing_images)
 
     class Meta:
-        abstract = True
         ordering = ('-created_at',)
-
-    """
-    Amenities: Features or amenities associated with the entire listing, applicable to all properties.
-    """
-
-
-class ListingImage(ImageBaseModel):
-    image = models.ImageField(null=False, blank=False, upload_to=listing_images)
-    listing = models.ForeignKey("listings.Listing", on_delete=models.CASCADE, related_name='images')
 
     def __str__(self):
         return f"{self.listing.title} image"
 
+
 class ListingProperty(models.Model):
+    """
+    Objects that are listed as a unit/1 e.g Entire plot listed for sale as a whole will have units as listing property
+    """
     listing = models.ForeignKey(
         'listings.Listing',
         on_delete=models.CASCADE,
@@ -56,14 +51,30 @@ class ListingProperty(models.Model):
     class Meta:
         ordering = ('-created_at',)
         verbose_name_plural = "Listing Properties"
+        unique_together = ('listing', 'property')
 
 
-class RentalListing(Listing):
+class RentalListing(models.Model):
+    """
+    Extends listing by adding specific information to rentals
+    Is a one to many allowing one property to be listed for several purposes e.g. as commercial, residential rentals,e.t.c
+    """
+    listing = models.ForeignKey("listings.Listing", on_delete=models.CASCADE, related_name='rentals')
     deposit_required = models.DecimalField(max_digits=10, decimal_places=2)
     renewal_interval = models.PositiveIntegerField()
 
 
-class SaleListing(Listing):
+class SaleListing(models.Model):
+    """
+    Extends listing by adding specific information to sales
 
+    """
+    listing = models.ForeignKey("listings.Listing", on_delete=models.CASCADE, related_name='sales')
+    down_payment_required = models.PositiveIntegerField(
+        null=True,
+        blank=True,
+        help_text="percentage of the property's total purchase price and demonstrates the buyer's commitment to the "
+                  "purchase and serves as an initial contribution toward the property"
+    )
     closing_date = models.DateField(null=True, blank=True)
     mortgage_options = models.TextField(null=True, blank=True)
